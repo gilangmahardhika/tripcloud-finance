1e4b9c02ddaf1cfb2e42d8ca790e9e5d8708ef5d
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-08-26 16:11:11 +0700

    add some model validations and add current_user

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,9 @@
 class Airline < ActiveRecord::Base
   attr_accessible :code, :logo, :name
+
+  validates_presence_of :code
+  validates_presence_of :name
+  validates_uniqueness_of :code
+
+  mount_uploader :logo, AirlineLogoUploader
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,7 @@
 class City < ActiveRecord::Base
   attr_accessible :code, :name
+
+  validates_presence_of :name
+  validates_presence_of :code
+  validates_uniqueness_of :code
 end
