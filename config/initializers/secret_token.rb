b1a39b218a5ea3575d0b8c1a6cecc66f3dd10190
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-08-26 15:42:22 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripcloudFinance::Application.config.secret_token = '39ce2e071d727b33022f0a143e4e57279669f95c9a8fb18f84e3c71637fbe99d80ea07abd33adfbfc465597d70f5d5dd151fc07706b84cb10fb3955872c8c7dd'
