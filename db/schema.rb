3593321cdca2583e34be5796e2f2086761b0bcff
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-09-11 15:33:48 +0700

    add address to customers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130911081240) do
+ActiveRecord::Schema.define(:version => 20130911083017) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130911081240) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "address"
   end
 
   add_index "customers", ["company"], :name => "index_customers_on_company"
