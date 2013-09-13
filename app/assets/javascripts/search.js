ced0b400be1f7134ee68ed673554eb3e14af92eb
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-09-13 16:28:44 +0700

    invoice search

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,4 @@
+//= require ./plugins/bootstrap-datepicker/js/bootstrap-datepicker
+//= require ./plugins/fancybox/source/jquery.fancybox.pack
+//= require ./scripts/app
+//= require ./scripts/search 
