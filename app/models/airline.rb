b8b40e9663bf0c6d5590434bb89b70d0dc118f35
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-09-26 18:01:49 +0700

    remove mandatory field on airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,9 +1,9 @@
 class Airline < ActiveRecord::Base
   attr_accessible :code, :logo, :name
 
-  validates_presence_of :code
+  # validates_presence_of :code
   validates_presence_of :name
-  validates_uniqueness_of :code
+  # validates_uniqueness_of :code
 
   has_many :transactions
   has_many :paxes
