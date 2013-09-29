1af70eb6b00cd1149e2ba5903fd3f233ce0e0eae
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-09-29 13:55:19 +0700

    remove unused assets on search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,3 @@
-//= require ./plugins/bootstrap-datepicker/js/bootstrap-datepicker
 //= require ./plugins/fancybox/source/jquery.fancybox.pack
 //= require ./scripts/app
 //= require ./scripts/search 
